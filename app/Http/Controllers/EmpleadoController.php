cdc165fb8aa0fe0a5d4d6344de594d6efa531cab
GK-Code <43577537+gabriel84k@users.noreply.github.com>
2021-11-16 19:07:39 -0300

    Add files via upload

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Personal;
+use App\Models\Red;
 
 class EmpleadoController extends Controller
 {
@@ -39,7 +40,43 @@ class EmpleadoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+        try {
+            \DB::beginTransaction();
+            $campos = request()->all();
+            $campos['temporal'] = (boolean)$campos['temporal'];
+            $campos['estado'] = (boolean)$campos['estado'];
+            unset($campos['user_id']) ;
+            $campos['empresa_id'] = 1;
+
+            $red = json_decode( $campos ['red']);
+            unset($campos['red']);
+            
+            $empleado = (new Personal);
+            $empleado = $empleado->create($campos);
+            
+            foreach ($red as $key => $value) {
+                $Reds = Red::where('personal_id',$empleado->id)
+                                ->where('puesto_id',$value->puesto->id)
+                                ->where('tarea_id',$value->tarea->id)
+                                ->first();
+                if($Reds){
+                    $Reds->update(['personal_id'=>$empleado->id,'puesto_id'=>$value->puesto->id,'tarea_id'=>$value->tarea->id]);    
+                }else{
+                    $Reds = (new Red);
+                    $Reds->create(['personal_id'=>$empleado->id,'puesto_id'=>$value->puesto->id,'tarea_id'=>$value->tarea->id]);    
+                }
+                                
+                
+               
+            }
+            \DB::commit();
+            return \Response::json(['status'=>0,'descripcion'=>'Empleado Creado con exito ','data'=>$empleado]);
+        } catch (\Throwable $th) {
+            \DB::rollback();
+            return \Response::json(['status'=>-1,'descripcion'=>'Error al crear el Empleados ','data'=>$th->getMessage().' Line'.$th->getLine()]);
+        }
+        
     }
 
     /**
