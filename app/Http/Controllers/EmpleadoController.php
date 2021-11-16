c8e5168f8838b6b70cdfad5b69c8fa31e5de6e9f
GK-Code <43577537+gabriel84k@users.noreply.github.com>
2021-11-16 14:41:11 -0300

    Add files via upload

---
+++ app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Shaka;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Personal;
+
+class EmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $empleado = Personal::with('tarea')->paginate(15);
+        return \Response::json(['status'=>0,'descripcion'=>'Todos los Empleados ','data'=>$empleado]);
+    }
+    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $empleado = Personal::with('tarea')->find($id);
+        return \Response::json(['status'=>0,'descripcion'=>'Detalle Empleados ','data'=>$empleado]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TareaController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Shaka;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\Tarea;
+
+class TareaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nombre' => 'required',
+            'descripcion' => 'required',
+            
+        ]);
+        if ($validator->fails()) {
+            return \Response::json(['status'=>-1,'errors'=>$validator->errors(),'descripcion'=>'']);
+        }
+
+        try {
+
+            $campos = request()->all();
+            $campos['estado'] = estado($campos['estado']);
+
+            $puesto = (new Tarea);
+            
+            $puesto->create($campos)->puesto()->sync($campos['puesto_id']);
+            
+
+            return \Response::json(['status'=>0,'descripcion'=>'Nueva Tarea agregado','data'=>\json_encode($puesto)]); 
+        } catch (\Throwable $th) {
+            return \Response::json(['status'=>-1,'descripcion'=>'Error En la Carga del Nuevo puesto','data'=>'error:'.$th->getMessage().' Linea:'. $th->getLine()]); 
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tarea = Tarea::find($id);
+        return \Response::json(['status'=>0,'descripcion'=>'Listado de Tarea','data'=>$tarea]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
